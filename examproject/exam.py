7bff05037f0a66f55271d293466f72c259e7bb30
nfv335 <nfv335@alumni.ku.dk>
2024-06-22 09:21:35 +0200

    Update exam.py

---
+++ examproject/exam.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from types import SimpleNamespace
+from scipy import optimize
+
+class Problem1():
+    def __init__(self, **kwargs):
+        '''
+        Set default parameters
+        '''
+        par = SimpleNamespace()
+
+        # firms
+        par.A = 1.0
+        par.gamma = 0.5
+
+        # households
+        par.alpha = 0.3
+        par.nu = 1.0
+        par.epsilon = 2.0
+
+        # government
+        par.tau = 0.0
+        par.T = 0.0
+
+        # Question 3
+        par.kappa = 0.1
+    
+    def 
